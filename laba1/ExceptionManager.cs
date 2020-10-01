25e89d51ed7701b5d70c5313f43869a188fc5061
Sofiia Obmok <sofiaobmok@gmail.com>
2020-10-01 16:44:26 +0300

    added ExceptionManager class

---
+++ laba1/ExceptionManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba1
+{
+    class ExceptionManager
+    {
+        public static bool isCritical(Exception e)
+        {
+            if (e is DivideByZeroException || e is NullReferenceException) return true;
+            return false;
+        }
+        public static void handle(Exception e)
+        {
+            var a = isCritical(e) ? critical++ : ordinary++;
+            Console.WriteLine("critical: " + critical);
+            Console.WriteLine("ordinary: " + ordinary);
+        }
+        private static int critical = 0;
+        private static int ordinary = 0;
+    }
+}
---
+++ laba1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ExceptionManager.handle(new DivideByZeroException());
+            ExceptionManager.handle(new InvalidOperationException());
+            Console.ReadKey();
+        }
+    }
+}
